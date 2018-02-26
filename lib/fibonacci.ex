0ea030f9909f034b62427db732814622b44f62b1
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-26 03:07:43 -0600

    Adding fibonacci exercise

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -2,6 +2,8 @@ defmodule Fibonacci do
 
   def findItem(0), do: 0
   def findItem(1), do: 1
-  def findItem(number), do: ( findItem(number-2) + findItem(number-1) )
+  def findItem(number) do
+    ( findItem(number-2) + findItem(number-1) )
+  end
 
 end
