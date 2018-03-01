9769c109748a5a7876fe68c1e2f3ee524db83592
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-01 02:22:18 -0600

    Adding new exercise

--- lib/nucleotides.ex
+++ lib/nucleotides.ex
@@ -4,6 +4,11 @@ defmodule Nucleotide do
   # 20 12 17 21
   # A', 'C', 'G', and 'T'
 
+  def process([], _), do: [{:a,0}, {:c,0}, {:g,0}, {:t,0}]
+  def process([h|t], counters) do
+
+  end
+
 end
 
 
