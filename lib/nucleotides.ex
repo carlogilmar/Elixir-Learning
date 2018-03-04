c1f5425ab49b709381b9c7b8c7f8c3e9fa1d07c0
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-03 23:01:56 -0600

    Adding my fucking exercise, I'm so happy 😇

--- lib/nucleotides.ex
+++ lib/nucleotides.ex
@@ -4,31 +4,19 @@ defmodule Nucleotide do
   # 20 12 17 21
   # A', 'C', 'G', and 'T'
 
-  def process([], _), do: [{:a,0}, {:c,0}, {:g,0}, {:t,0}]
-  def process([h|t], counters) do
-
+  def start( nucleotide ) do
+    String.codepoints(nucleotide)
+      |> get( %{"A"=>0, "C"=>0, "G"=>0, "T"=>0} )
   end
 
-
-  def start() do
-    get(["A","B","B","A","A","B"], %{"A"=>0,"B"=>0})
-  end
-
-  def get([h|t], c) do
-    increment(h, t, c)
-  end
-
+  def get([h|t], counters), do: increment(h, t, counters)
   def get([], counters), do: IO.inspect counters
 
   def increment(element, tail, counters) do
     currentCounter = counters[element] + 1
     countersUpdated = Map.put(counters, element, currentCounter)
-    IO.inspect countersUpdated
     get( tail, countersUpdated)
   end
-
-  def increment(_, [], counters), do: :end_of_list
-
 
 end
 
