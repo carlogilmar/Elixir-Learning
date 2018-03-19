fa961554307a2d299f082671ea2b65e57feed208
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-19 00:54:27 -0600

    Adding first attemp

--- lib/inverseCaptcha.ex
+++ lib/inverseCaptcha.ex
@@ -9,6 +9,29 @@ defmodule InverseCaptcha do
     {parsedInteger,_} = Integer.parse(h)
     [ parsedInteger ] ++ parserToInteger(t)
   end
+
   def parserToInteger( [] ), do: []
+
+  def getSumList([e], sumList) do
+    sumList
+  end
+  def getSumList( elementList, sumList) do
+    [ element | t ] = elementList
+    [ next | _ ] = t
+    suscribeElement({ element == next, element, t, sumList })
+  end
+
+  def suscribeElement({true, element, list, sumList}) do
+    getSumList( list, sumList ++ [element])
+  end
+  def suscribeElement({false, _, list, sumList}) do
+    IO.puts("can u see me?")
+    IO.inspect( list )
+    getSumList( list, sumList)
+  end
+  def suscribeElement({_, _, [], sumList}) do
+    IO.puts("Terminamos la lista ==============>")
+    getSumList( [], sumList)
+  end
 
 end
