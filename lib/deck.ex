2b622bfd2503e80eb00240cbc63cff7143fb43f6
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-31 02:02:12 -0600

    Adding example deck with genserver2

---
+++ lib/deck.ex
@@ -0,0 +1,26 @@
+defmodule Deck do
+
+	use GenServer
+
+	def start_link() do
+		GenServer.start_link(__MODULE__, [], name: __MODULE__)
+	end
+
+	def take_card() do
+		GenServer.call(__MODULE__, {:take_card})
+	end
+
+	def init(_) do
+		{:ok, Enum.shuffle(1..52)}
+	end
+
+	def handle_call({:take_card}, _from, [card|deck]) do
+		{:reply, card, deck}
+	end
+
+	def start do
+		Deck.start_link
+		1..51 |> Enum.each( fn(_) -> IO.puts(Deck.take_card) end )
+	end
+
+end
