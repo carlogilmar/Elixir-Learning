2c6ec27c781d8c79d6a91030e7a1531e2484cfba
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-30 22:25:12 -0600

    Adding the word Counter using streams

---
+++ lib/wordCounterStreams.ex
@@ -0,0 +1,21 @@
+defmodule Words do
+	
+	def count(lines, mod) do
+		lines 
+		|> mod.flat_map(&String.split/1)
+		|> mod.map(&String.downcase/1)
+		|> mod.map(&remove_special_chars/1)
+		|> Enum.reduce(%{}, &count_word/2)		
+	end
+
+	defp remove_special_chars(string) do
+		string
+		|> String.normalize(:nfd)
+		|> String.replace(~r/[^A-z\s]/u, "")
+	end
+
+	defp count_word(word, map) do
+		Map.update(map, word, 1, &(&1 + 1))
+	end
+
+end
