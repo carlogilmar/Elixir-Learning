9da4e443417e230c99f0bad78218258acc94b9c5
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-01 20:26:56 -0500

    Refactor

--- lib/facebook/exercises.ex
+++ lib/facebook/exercises.ex
@@ -1,35 +1,48 @@
 defmodule FbExercises do
-  def generate_matrix(number) do
-    limit = get_numbers_for_fill_matrix.(number)
-    numbers = Enum.to_list(1..limit)
-    fill_matrix(numbers, number)
+  def generate_matrix(pivot) do
+    limit = get_numbers_for_fill_matrix.(pivot)
+    numbers_for_fill = Enum.to_list(1..limit)
+    fill_matrix(numbers_for_fill, pivot)
   end
 
-  def fill_matrix(numbers, number) do
-    empty_lists = for _n <- Enum.to_list(1..number), do: []
-    first_list = Enum.take(numbers, number)
-    numbers_available = numbers -- first_list
-    list_with_end = fill_last_element_in_lists(empty_lists, numbers_available)
-    last_list = fill_last_list(list_with_end, number)
+  def fill_matrix(numbers_for_fill, pivot) do
+
+    empty_lists = for _n <- Enum.to_list(1..pivot), do: []
+
+    {first_list, numbers_available} = get_first_list(numbers_for_fill, pivot)
+
+    {tail, numbers_available} = fill_last_element_in_lists(empty_lists, numbers_available)
+
+    {last_list, numbers_available} = fill_last_list({tail, numbers_available}, pivot)
+
     [first_list, [8,9,4], last_list]
   end
 
-  def fill_last_list({lists, numbers}, number) do
+  def get_first_list(numbers_for_fill, pivot) do
+    first_list = Enum.take(numbers_for_fill, pivot)
+    numbers_available = numbers_for_fill -- first_list
+    {first_list, numbers_available}
+  end
+
+  def fill_last_list({lists, numbers_available}, number) do
     [last_list|_t] = Enum.reverse(lists)
     indexes = Enum.to_list(1..(number-1))
-    tail_last_list = for {_index, number} <- Enum.zip(indexes, numbers), do: number
-    Enum.reverse(tail_last_list) ++ last_list
+    tail_last_list = for {_index, number} <- Enum.zip(indexes, numbers_available), do: number
+    elements_in_list = Enum.reverse(tail_last_list)
+    numbers_available = numbers_available -- elements_in_list
+    last_list = elements_in_list ++ last_list
+    {last_list, numbers_available}
   end
 
   def fill_last_element_in_lists(matrix, numbers) do
     size = length(matrix)
-    numbers_not_used = numbers -- (Enum.take(numbers, size-1))
+    numbers_available = numbers -- (Enum.take(numbers, size-1))
     [_list|tail] = matrix
     next_lists =
       for {list, number} <- Enum.zip(tail, numbers) do
         list ++ [number]
       end
-    {next_lists, numbers_not_used}
+    {next_lists, numbers_available}
   end
 
   def get_numbers_for_fill_matrix do
--- test/exercises_test.exs
+++ test/exercises_test.exs
@@ -7,4 +7,17 @@ defmodule ExercisesTest do
     matrix = FbExercises.generate_matrix(number)
     assert matrix == expected_matrix
   end
+
+  #test "get a matrix with 4 " do
+  #  number = 4
+  #  expected_matrix =
+  #    [
+  #      [1,2,3,4],
+  #      [12,13,14,5],
+  #      [11,16,15,6],
+  #      [10, 9, 8, 7]
+  #    ]
+  #  matrix = FbExercises.generate_matrix(number)
+  #  assert matrix == expected_matrix
+  #end
 end
